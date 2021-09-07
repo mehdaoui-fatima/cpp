0537b5e0f94a3cb2ddcb9c3cc72345b25e556d55
mehdaoui-fatima <mehdaoui.fa@gmail.com>
2021-09-07 14:46:58 +0100

    ex01 done

--- day02/ex00/Fixed.cpp
+++ day02/ex00/Fixed.cpp
@@ -6,7 +6,7 @@
 /*   By: fmehdaou <fmehdaou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/31 16:08:08 by fmehdaou          #+#    #+#             */
-/*   Updated: 2021/09/07 11:37:18 by fmehdaou         ###   ########.fr       */
+/*   Updated: 2021/09/07 14:40:58 by fmehdaou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,6 @@ Fixed::Fixed( Fixed const &old_obj )
 {
     std::cout << "Copy constructor called" << std::endl;
     this->fixedPoint = old_obj.fixedPoint;
-    
 }
 
 int Fixed::getRawBits( void ) const
@@ -40,7 +39,8 @@ void Fixed::setRawBits( int const raw )
 Fixed& Fixed::operator=(Fixed const & rhs) 
 {
     std::cout << "Assignation operator called" << std::endl;
-    this->fixedPoint = rhs.getRawBits();
+    if (this != &rhs)
+        this->fixedPoint = rhs.getRawBits();
     return *this;
 }
 
--- day02/ex00/main.cpp
+++ day02/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: fmehdaou <fmehdaou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/31 16:08:19 by fmehdaou          #+#    #+#             */
-/*   Updated: 2021/09/07 11:02:52 by fmehdaou         ###   ########.fr       */
+/*   Updated: 2021/09/07 14:46:44 by fmehdaou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,8 @@
 
 int main( void ) 
 {
-
     Fixed a;
-    Fixed b( a ); // 
+    Fixed b( a );
     Fixed c;
     
     c = b;
